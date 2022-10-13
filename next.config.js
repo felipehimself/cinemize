cf00c00e3446dd94db2d103cb0373802eab1fbe3
Felipe Couto <felipecoutodarocha@gmail.com>
2022-10-13 18:49:29 -0300

    Ajuste no arquivo nextjs.config.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/notfound',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
